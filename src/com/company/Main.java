2c86f967eda3f918c32668193573e2959e1b5687
Taylor Millett <tmillett@adobe.com>
2017-04-17 10:56:23 -0700

    handle args into main method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,17 +4,29 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String trainingDataPath;
         String validationDataPath = null;
 
-        //String trainingDataPath = "/Users/tmillett/_dev/git/uw/machine_learning/_proj1/training_tennis.arff";
-        //validationDataPath = "/Users/tmillett/_dev/git/uw/machine_learning/_proj1/testing_tennis.arff";
+        if (args.length > 0) {
+            trainingDataPath = args[0];
+            if (args.length > 1) {
+                validationDataPath = args[1];
+            }
+        } else {
+            //String trainingDataPath = "/Users/tmillett/_dev/git/uw/machine_learning/_proj1/training_tennis.arff";
+            //validationDataPath = "/Users/tmillett/_dev/git/uw/machine_learning/_proj1/testing_tennis.arff";
 
 
-        //String trainingDataPath = "/Users/tmillett/_dev/git/uw/machine_learning/_proj1/training_tennis_missing.arff";
-        //validationDataPath = "/Users/tmillett/_dev/git/uw/machine_learning/_proj1/testing_tennis_missing.arff";
+            //String trainingDataPath = "/Users/tmillett/_dev/git/uw/machine_learning/_proj1/training_tennis_missing.arff";
+            //validationDataPath = "/Users/tmillett/_dev/git/uw/machine_learning/_proj1/testing_tennis_missing.arff";
 
-        String trainingDataPath = "/Users/tmillett/_dev/git/uw/machine_learning/_proj1/training_subsetD.arff";
-        validationDataPath = "/Users/tmillett/_dev/git/uw/machine_learning/_proj1/testingD.arff";
+            trainingDataPath = "/Users/tmillett/_dev/git/uw/machine_learning/_proj1/training_subsetD.arff";
+            validationDataPath = "/Users/tmillett/_dev/git/uw/machine_learning/_proj1/testingD.arff";
+        }
+
+
+
+
         ID3DecisionTree tree = new ID3DecisionTree(trainingDataPath, validationDataPath);
         tree.evaluate();
 
